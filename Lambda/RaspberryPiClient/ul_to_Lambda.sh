733d98435fa8a74ffc938a7a7e97bfeb51fa90e5
nilesflow <nilesflow@gmail.com>
2018-09-30 02:33:31 +0900

    ・LambdaへのアップロードでSSL証明書を追加 ・不要ファイルを含めないように修正

--- Lambda/RaspberryPiClient/ul_to_Lambda.sh
+++ Lambda/RaspberryPiClient/ul_to_Lambda.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-git archive HEAD --output=Lambda-RaspberryPiClient.zip
+rm -f Lambda-RaspberryPiClient.zip
+git archive HEAD --output=Lambda-RaspberryPiClient.zip --worktree-attributes
+
+# 証明書があるか確認
+if [ -e certs ]; then
+    :
+else
+    echo "certs/ 配下にSSL証明書を指定してください。"
+    exit
+fi
+zip -r Lambda-RaspberryPiClient.zip certs
+
 aws lambda update-function-code --function-name RaspberryPiClient --zip-file fileb://Lambda-RaspberryPiClient.zip --publish
